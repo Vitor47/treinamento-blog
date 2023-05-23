494c2cb6dbf8c5a5204b6d7c53fc1920513d1cc6
Vitor Miolo <vitor.miolo@gestaods.com.br>
2023-05-23 00:18:12 -0300

    first commit

---
+++ apps/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    titulo = models.CharField(max_length=150)
+    descricao = models.TextField(null=True, blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Blog da Amf"
+        verbose_name_plural = "Blogs da AMF"
+
+    def __str__(self):
+        return self.titulo
