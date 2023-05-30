6c9f1b94a97ace93d90285c12fcaa3c15a7a24f0
Vitor Miolo <vitor.miolo@gestaods.com.br>
2023-05-29 22:36:13 -0300

    templates e imagem

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Blog
 
+
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ("id", "titulo", )
+    def imagem_tag(self, obj):
+        if obj.imagem:
+            return format_html(f"<img src='{obj.imagem.url}'  width='100' height='80' />")
+        return ""
+
+    imagem_tag.short_description = "Imagens"
+
+    list_display = ("id", "titulo", "imagem_tag", )
+    list_display_links = ("titulo", )
     list_filter = ("titulo", )
     search_fields = ("titulo", )
+
 
 admin.site.register(Blog, BlogAdmin)
--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('apps.blog.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
